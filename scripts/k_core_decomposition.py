cf7d72e148f9c7c92902e902a3832c1b5196360f
Sadamori Kojaku <skojaku@iu.edu>
2023-10-09 23:31:25 -0400

    assignment

--- scripts/k_core_decomposition.py
+++
@@ -1,8 +0,0 @@
-def kcore_decomposition(g, k):
-    """K-core decomposition
-
-    Return:
-    node_indiced: Indices of nodes in the k-core
-    """
-
-    return node_indices
--- tests/test.py
+++ tests/test.py
@@ -14,36 +14,3 @@ class Test(unittest.TestCase):
         A = nx.adjacency_matrix(G)
         src, trg, _ = sparse.find(sparse.triu(A, 1))
         self.g = igraph.Graph(tuple(zip(src, trg)))
-
-    def test_random_attack(self):
-        x, y = random_attack(self.g)
-
-        assert np.all(np.isclose(np.diff(x), 1.0 / self.g.vcount()))
-        assert len(x) == len(y)
-
-        rindex = np.mean(y)
-
-        assert rindex >= 0.3
-        assert rindex <= 0.5
-
-    def test_targeted_attack(self):
-        x, y = degree_targeted_attack(self.g)
-        assert np.all(np.isclose(np.diff(x), 1.0 / self.g.vcount()))
-        assert len(x) == len(y)
-        rindex = np.mean(y)
-        assert np.isclose(rindex, 0.16050420168067225)
-
-    def test_betweenness_attack(self):
-        x, y = betweenness_targeted_attack(self.g)
-        assert np.all(np.isclose(np.diff(x), 1.0 / self.g.vcount()))
-        assert len(x) == len(y)
-        rindex = np.mean(y)
-        assert np.isclose(rindex, 0.1588235294117647)
-
-    def test_kcore(self):
-        node_indices = kcore_decomposition(self.g, k=4)
-
-        assert np.all(
-            np.sort(node_indices)
-            == np.sort(np.where(np.array(self.g.coreness()) == 4)[0])
-        )
